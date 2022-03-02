536fef5975caf5b034263c1f36f5f39351a2fd81
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-02 10:28:07 +0530

    interpolation only.!!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-// Writing HTML in JavaScript :: WOW
-// JSX (JavaScript XML)
-// 1. Root Tags 2. Tags must be closed. 3. Tags must be properly nested.
+// Interpolation ::
 function App() {
-  return (
-    <div>
-      <h1>Russia Ukrian War </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eos
-        suscipit amet molestiae nesciunt veritatis non! In molestiae sed sequi!
-        Esse vitae molestiae recusandae nihil atque dolore beatae ipsa deserunt.
-      </p>
-    </div>
-  );
+  let name = "John";
+
+  return <h1>Hello, {name} </h1>;
 }
 
 export default App;
