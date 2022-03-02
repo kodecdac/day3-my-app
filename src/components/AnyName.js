a4211a57b7c3c26f156f6448e93bddb68e38e839
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-02 11:24:13 +0530

    external componnents.

---
+++ src/components/AnyName.js
@@ -0,0 +1,19 @@
+export default function AnyName() {
+  return (
+    <div>
+      <h1>Subject</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo similique
+        facilis nostrum aperiam natus molestiae atque ipsa amet optio dolorum
+        quod unde laudantium maiores a culpa, ab, accusantium quas. Nam minima
+        cum rem numquam dolorem sint quia? Adipisci asperiores in exercitationem
+        id totam. Officiis eligendi error sequi culpa perspiciatis? Perspiciatis
+        quaerat vero eveniet odit, quisquam iusto rem. Excepturi vitae impedit
+        aliquid nulla optio ipsum laboriosam et natus, repellat ratione
+        distinctio adipisci, in autem, fugit blanditiis repellendus beatae modi?
+        Ipsa provident nulla eveniet vel repellendus ipsam veniam quasi, omnis
+        eos vero optio illum ab dolor enim fuga quam animi neque eius!
+      </p>
+    </div>
+  );
+}
---
+++ src/components/ManyName.js
@@ -0,0 +1,20 @@
+export default function ManyName() {
+  return (
+    <div>
+      <h1>Questionaries</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        eveniet, ut magnam ipsum quis quia porro! Ullam dicta maiores enim
+        consequuntur officiis. Omnis iusto officia fugit laudantium beatae
+        animi, quo numquam tenetur odit id delectus similique corporis explicabo
+        ut eveniet deserunt quae ipsa sed debitis dolor distinctio quod. Libero
+        voluptate alias rerum repudiandae odit unde accusantium odio similique
+        quasi harum vero blanditiis rem, cum neque tempore minus praesentium
+        facilis, et voluptatibus nesciunt iusto? Explicabo reiciendis dicta
+        alias voluptate deleniti ab ducimus cum quasi mollitia quis cupiditate
+        pariatur nostrum quas doloremque doloribus in, autem officia? Soluta
+        quasi nam quidem quibusdam corrupti.
+      </p>
+    </div>
+  );
+}
