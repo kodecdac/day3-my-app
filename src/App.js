40bc6916861013d7b3803fdf0383c5c047968c9d
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-02 10:39:07 +0530

    b6 - databinding, focussed on object,

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 // Interpolation ::
 function App() {
   let name = "John";
+  let city = "New York";
+  let age = 30;
+  let isMarried = false;
 
-  return <h1>Hello, {name} </h1>;
+  // IMPORTANT
+  let user = { username: "rahul", city: "mumbai" };
+  let userr = undefined;
+
+  return (
+    <h1>
+      Hello, {name} frommmm {city}!
+      <br />
+      You are {age} years old.
+      <br />
+      You are {isMarried ? "Married" : "UnMarried"}.
+      <br />
+      {user.username} is from {user.city}.
+      <br />
+      {userr?.username}
+    </h1>
+  );
 }
 
 export default App;
