ccbddd28f91aea2600d8f4a3f3dc94d2b6a17e09
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-02 10:19:24 +0530

    b4 - parent tag or root tag.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 // Writing HTML in JavaScript :: WOW
 // JSX (JavaScript XML)
+// 1. Root Tags 2. Tags must be closed. 3. Tags must be properly nested.
 function App() {
-  return <h1>Hello World, Congratulations!!</h1>;
+  return (
+    <div>
+      <h1>Russia Ukrian War </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eos
+        suscipit amet molestiae nesciunt veritatis non! In molestiae sed sequi!
+        Esse vitae molestiae recusandae nihil atque dolore beatae ipsa deserunt.
+      </p>
+    </div>
+  );
 }
 
 export default App;
