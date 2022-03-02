d7938b610a3ca0fe6d803652a2a31f9a81e45d8f
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-02 11:16:48 +0530

    working with multiple componens.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 export default function App() {
   return (
     <div>
-      <AnyName name="Russian war" />
-      <AnyName name="Americal War" />
-      <AnyName name="Indo War" />
+      <AnyName name="CDAC" id="100" />
+      <ManyName />
+      <ManyName />
+      <AnyName />
     </div>
   );
 }
 
-function AnyName({ name }) {
+function AnyName({ name = "Default", id = 1 }) {
   return (
     <div>
       <h1>{name}</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+        {id} Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
         reiciendis, nisi dolores quis nesciunt sit quibusdam, aspernatur nam
         earum perspiciatis at! Reiciendis voluptate nisi saepe illum
         necessitatibus ea mollitia natus rerum commodi blanditiis! Quisquam
@@ -26,6 +27,19 @@ function AnyName({ name }) {
         ratione qui eligendi obcaecati! Mollitia praesentium culpa velit earum
         tenetur. Maxime nam reprehenderit repellendus!
       </p>
+    </div>
+  );
+}
+
+function ManyName() {
+  return (
+    <div>
+      <h1>Questionaries</h1>
+      <div>
+        <input type="text" name="" id="" placeholder="Enter your answer" />{" "}
+        <br />
+        <input type="text" name="" id="" placeholder="Opetion1s" />
+      </div>
     </div>
   );
 }
