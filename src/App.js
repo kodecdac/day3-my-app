b385f112fdfe93be4f50e642951a324d4a762f08
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-02 10:56:40 +0530

    udc

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
       <AnyName />
     </div>
   );
 }
 
-// NOTE :: CaptialCase
-// html, body, h1, div
-// UserDefined Tag :: Component :: <AnyName />
 function AnyName() {
-  return <div>Hello I am Any!!</div>;
+  return (
+    <div>
+      <h1>Russian Warr</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+        reiciendis, nisi dolores quis nesciunt sit quibusdam, aspernatur nam
+        earum perspiciatis at! Reiciendis voluptate nisi saepe illum
+        necessitatibus ea mollitia natus rerum commodi blanditiis! Quisquam
+        officiis architecto dolor sunt, quia possimus excepturi accusantium
+        eveniet molestias dignissimos labore dolores doloribus maxime nemo ullam
+        incidunt sint enim atque blanditiis. Repudiandae dolore quam pariatur
+        incidunt. Aliquam, repellendus. Consequatur provident eos doloribus
+        voluptates iusto, esse soluta dolores, praesentium temporibus blanditiis
+        neque maiores nulla quia repellat delectus tempora accusamus, ut unde a
+        ratione qui eligendi obcaecati! Mollitia praesentium culpa velit earum
+        tenetur. Maxime nam reprehenderit repellendus!
+      </p>
+    </div>
+  );
 }
